897484b9031c66cfea67040c6330145ecc576e74
Mohammad-Shamel Agha <ma2298@njit.edu>
2022-03-30 17:14:19 -0400

    Update: Change from Get to Post

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -24,7 +24,7 @@ def test_auth_pages(client):
 # Unit Test 1 - Bad password (Login)
 def test_invalid_password(client):
     """ Unit Test for Incorrect Password for Login """
-    response = client.get("/login")
+    response = client.post("/login")
     test_user = 'IS219_TestUser@email.com'
     test_password = 'invalid_password'
     if User.email == test_user:
@@ -35,7 +35,7 @@ def test_invalid_password(client):
 # Unit Test 2 - Bad username / email  (login)
 def test_invalid_email_address(client):
     """ Unit Test for Incorrect Email for Login"""
-    response = client.get("/login")
+    response = client.post("/login")
     if User.email is None:
         assert 'Invalid username' in response.data
 
@@ -43,18 +43,18 @@ def test_invalid_email_address(client):
 # Unit Test 3 - Bad username / email (Registration)
 def test_invalid_email_address_register(client):
     """ Unit Test for Invalid Email for Registration"""
-    response = client.get("/register")
+    response = client.post("/register")
     test_email = 'test'
     if User.email is None:
         User.email = test_email
         if '@' not in User.email:
-            assert 'Please include an '@' in the email address'
+            assert 'Please include an '@' in the email address' in response.data
 
 
 # Unit Test 4 - Password Confirmation (registration)
 def test_password_confirmation(client):
     """ Unit Test for Password Confirmation"""
-    response = client.get("/register")
+    response = client.post("/register")
     test_password = 'Dummy_Pass_123'
     if User.email is not None:
         if User.password == test_password:
@@ -64,7 +64,7 @@ def test_password_confirmation(client):
 # Unit Test 5 - Bad Password - does not meet criteria (registration)
 def test_invalid_password_registration(client):
     """ Unit Test for Password Criteria Check"""
-    response = client.get("/register")
+    response = client.post("/register")
     test_pass = 'bad'
     if User.email is None:
         User.password = test_pass
@@ -84,7 +84,7 @@ def test_already_registered(client):
 # Unit Test 7 - Successful Login
 def test_successful_login(client):
     """ Unit Test for Successful Login"""
-    response = client.get("/login")
+    response = client.post("/login")
     test_user = 'IS219_TestUser@email.com'
     test_password = 'Dummy_Pass_123'
     if User.email == test_user:
@@ -95,7 +95,7 @@ def test_successful_login(client):
 # Unit Test 8 - Successful Registration
 def test_successful_registration(client):
     """ Unit Test for Successful Registration"""
-    response = client.get("/register")
+    response = client.post("/register")
     if User is None:
         assert 'Congratulations, you are now a registered user!' in response.data
 
@@ -103,7 +103,7 @@ def test_successful_registration(client):
 # Unit Test 9 - denying access to the dashboard for logged out users
 def test_deny_dashboard(client):
     """ Unit Test for Denying Access to Dashboard"""
-    response = client.get("/dashboard")
+    response = client.post("/dashboard")
     if not User.authenticated:
         assert 'User Not Authenticated' in response.data
 
@@ -111,7 +111,7 @@ def test_deny_dashboard(client):
 # Unit Test 10 - allowing access to the dashboard for logged in users
 def test_allow_dashboard(client):
     """ Unit Test for Allowing Access to Dashboard"""
-    response = client.get("/dashboard")
+    response = client.post("/dashboard")
     test_user = 'IS219_TestUser@email.com'
     test_password = 'Dummy_Pass_123'
     if User.email == test_user and User.password == test_password:
